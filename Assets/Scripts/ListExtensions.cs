c837ff4eb7361fd37d5bb371805556d744e6ae75
EvgeniyAA <andreeff.ea@yandex.ru>
2016-09-08 02:50:44 +0300

    modified AI logic. Now AI uses enum state types. Still have some bugs C:

---
+++ Assets/Scripts/ListExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ListExtensions
+{
+    public static T Minimum<T>(this List<T> list, Func<T, float> selector)
+    {
+        T min = list[0];
+        foreach (T item in list)
+        {
+            float value = selector(item);
+            if (selector(item) < selector(min))
+            {
+                min = item;
+            }
+        }
+        return min;
+    }
+}
+
---
+++ Assets/Scripts/StateType.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts
+{
+    public enum StateType
+    { 
+        Nothing,
+        SearchForCube,
+        RotateToCube,
+        MoveToCube,
+        PickUpCube,
+        RotateToPlayer,
+        MoveToPlayer,
+        MakeAShot
+    }
+}
